caaec5510508c23ae501d4442b2bbb1ef27d77b5
Fernando de Lima Rodrigues <fernando_tecti@hotmail.com>
2024-11-12 23:10:50 -0300

    Adicionado as dependências do projeto front e desenvolvimento do header da página inicial

---
+++ resources/views/statics/home.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <title>Kombi</title>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div id="header" class="d-flex justify-content-between align-items-center">
+                <a href="">
+                    <img src="{{ asset('assets/img/logo.png') }}" alt="logo">
+                </a>
+                <nav class="d-flex align-items-center">
+                    <ul class="d-flex list-unstyled mb-0 align-items-center">
+                        <li>
+                            <a class="nav-link" href="#servicos">Nossos serviços</a>
+                        </li>
+                        <li>
+                            <a class="nav-link" href="#sobre">Sobre nós</a>
+                        </li>
+                        <li>
+                            <a class="button-link" href="">SOLICITE UM ORÇAMENTO</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+    <main>
+        <section id="banner">
+            
+        </section>
+
+    </main>
+    <footer>
+
+    </footer>
+
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/js/script.js') }}"></script>
+</body>
+
+</html>
