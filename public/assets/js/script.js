3bfa5b6cde8ac266a34a617cb5cb75c84bd6c52e
Fernando de Lima Rodrigues <fernando_tecti@hotmail.com>
2024-11-14 23:42:17 -0300

    Adicionado swipper no projeto para funcionamento do carousel do banner

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,1 +1,29 @@
+var swiper = new Swiper(".mySwiper", {
+    loop: true, // Faz com que o slide seja infinito
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 6000, // Tempo entre os slides (em milissegundos)
+        disableOnInteraction: false,
+    },
+    speed: 1300, // Tempo de transicao entre os slides (em milissegundos)
+});
+
+let btn_home = document.querySelector("#home_link");
+
+
+
+btn_home.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (window.scrollY == 0) {
+        console.log('Você já está na página inicial')
+        location.reload();
+    } else {
+        window.scrollTo(0, 0);
+    }
+    e.preventDefault();
+    return false;
+});
 
--- resources/views/statics/home.blade.php
+++ resources/views/statics/home.blade.php
@@ -6,6 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1:wght@100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/css/swiper-bundle.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <title>Kombi</title>
 </head>
@@ -14,7 +18,7 @@
     <header>
         <div class="container">
             <div id="header" class="d-flex justify-content-between align-items-center">
-                <a href="">
+                <a href="#" id="home_link">
                     <img src="{{ asset('assets/img/logo.png') }}" alt="logo">
                 </a>
                 <nav class="d-flex align-items-center">
@@ -35,15 +39,69 @@
     </header>
     <main>
         <section id="banner">
-            
+            <div class="swiper mySwiper">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <img src="{{ asset('assets/img/banner.png') }}" alt="Banner 1">
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="{{ asset('assets/img/banner.png') }}" alt="Banner 1">
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="{{ asset('assets/img/banner.png') }}" alt="Banner 1">
+                    </div>
+                </div>
+                <!-- Paginação -->
+                <div class="swiper-pagination"></div>
+            </div>
         </section>
-
+        <section id="servicos">
+            <div class="bg-primary">
+                <div>
+                    <div>A farmácia de manipulação</div>
+                    <div>sob medida para você e seu pet</div>
+                </div>
+                <hr>
+            </div>
+        </section>
+        <section>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae cupiditate aliquid veniam placeat, enim adipisci officiis dolor neque expedita quas. Accusantium, animi molestias? Consectetur et esse dolorem fugit quibusdam.
+        </section>
     </main>
     <footer>
 
     </footer>
 
     <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/js/swiper-bundle.min.js') }}"></script>
     <script src="{{ asset('assets/js/script.js') }}"></script>
 </body>
 
